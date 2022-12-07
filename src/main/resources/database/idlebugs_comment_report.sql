2061da3eaa8c02921eb136e6a09c926b7545311f
Silvia <15023199401@qq.com>
2022-12-07 11:11:43 +0800

    0.0.0 database update

---
+++ src/main/resources/database/idlebugs_comment_report.sql
@@ -0,0 +1,15 @@
+create table comment_report
+(
+    id         int auto_increment
+        primary key,
+    comment_id int            not null comment '被舉報的留言id',
+    phone      varchar(255)   not null comment '舉報者的手機',
+    content    varchar(10000) not null comment '舉報原因',
+    date       datetime       not null,
+    status     int            not null comment '0: 還沒處理
+1: pass (留言違規)
+2: reject (留言沒有違規)',
+    constraint comment_report_id_uindex
+        unique (id)
+);
+
