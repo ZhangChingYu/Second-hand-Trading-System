c4c371d27294a91690f13ec7b82cc6465a63e37f
TaroPu <2735517752@qq.com>
2022-12-18 00:15:07 +0800

    分类管理

--- pages/main/index.vue
+++ pages/main/index.vue
@@ -1,6 +1,8 @@
 <template>
-	<view>
-		
+	<view class="home">
+		<view class="title">
+			<text>闲置虫虫二手交易服务端主页</text>
+		</view>
 	</view>
 </template>
 
@@ -17,6 +19,16 @@
 	}
 </script>
 
-<style>
-
+<style scoped lang="less">
+	 .home {
+		 min-height: 100vh;
+	 }
+	 .title {
+		 width: 14em;
+		 font-size: 40rpx;
+		 color: #b63220;
+		 margin: 100rpx auto;
+		 font-weight: 600;
+		 text-shadow: 5rpx 5rpx 3rpx rgba(0, 0,0 ,.3);
+	 }
 </style>
